421ac739c58199a9082bb6a14aeb893708b906d8
Antoine Ludwig <100688843+AntoineLudwigGalli@users.noreply.github.com>
2022-05-19 10:01:19 +0200

    affichage des 3 derniers articles sur la page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +16,19 @@ class MainController extends AbstractController
      * Contrôleur de la page d'accueil
      */
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
+        $articleRepo = $doctrine->getRepository(Article::class);
+        $articles = $articleRepo->findBy(
+            [],
+            ['publicationDate' => 'DESC'],
+            $this->getParameter("app.articles.last_article_number_on_home"),
+        );
 
 
-        return $this->render('main/home.html.twig');
+        return $this->render('main/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     /**
